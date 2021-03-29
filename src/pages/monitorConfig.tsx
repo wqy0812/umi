c8254eb3fb13fe3531918a59abb4ba741cb8a3ce
王启宇 <wangqy0812@gmail.com>
2021-03-29 11:35:48 +0800

    日

--- src/pages/monitorConfig.tsx
+++ src/pages/monitorConfig.tsx
@@ -1,31 +1,19 @@
 import styles from './common.less';
 import { Button, Divider, Select, Modal, Input, InputNumber } from 'antd';
-import React, { useState } from 'react';
+import React from 'react';
 import request from 'umi-request';
-// import { useModel } from 'umi';
 
 const { Option } = Select;
-
-//JSON数据结构
-// class Clusters {
-//   NodeIps: string[];
-//   ClusterName: string;
-// }
-//
-// class ClusterJsons {
-//   Clusters: Clusters[];
-// }
 
 //集群下拉框
 class ClusterSelect extends React.Component<{
   clusterList?: string[];
-  selectedCluster?: string;
 }> {
   constructor(props) {
     super(props);
     this.state = {
       clusterList: [],
-      selectedCluster: '',
+
     };
   }
 
@@ -46,104 +34,126 @@ class ClusterSelect extends React.Component<{
       });
   }
 
-  onChange(value) {
-    // this.setState({
-    //   selectedCluster: value
-    // });
-    this.props.onChange('dateCommenced', dateValue);
-  }
+  handleCurrencyChange = currency => {
+    if (!('value' in this.props)) {
+      this.setState({ currency });
+    }
+    this.triggerChange({ currency });
+  };
+
+  triggerChange = changedValue => {
+    // Should provide an event to pass value to Form.
+    const { onChange } = this.props;
+    if (onChange) {
+      onChange({
+        ...this.state,
+        ...changedValue,
+      });
+    }
+  };
 
   render() {
     return (
       <div>
-        <Select
-          defaultValue="集群名称"
-          style={{ width: 150 }}
-          onChange={this.onChange.bind(this)}
-        >
+        <Select defaultValue="集群名称" style={{ width: 150 }} onChange={this.handleCurrencyChange}>
           {(this.state['clusterList'] || []).map((item) => {
             return (
               <Option key={item} value={item}>
                 {item}
               </Option>
-            );
-          })}
+            )})}
         </Select>
       </div>
     );
   }
 }
 
-export default () => {
+export default class MonitorConfigPage extends React.Component{
   // const { initialState } = useModel('@@initialState');
   // console.log({initialState});
-
-  function onInterValChange(value) {
-    this.setState({
-      interVal: value,
-    });
+  constructor(props) {
+    super(props);
+    this.state = {
+      isModalVisible: false,
+      fssExporterInterval: 0,
+      selectedCluster: "1",
+    };
   }
 
-  const [isModalVisible, setIsModalVisible] = useState(false);
-
-  const showModal = () => {
+  showModal = () => {
+    //todo:get cluster and inter val
     request
-      .get('/updateFssExporterConfig?clusterName=' + 'wqy' + '&interval=' + '2')
+      .get('/updateFssExporterConfig?clusterName=' + 'wqy' + '&fssExporterInterval=' + '2')
       .then((response) => {
         console.log(response);
       })
       .catch(function (error) {
         console.log(error);
       });
-    setIsModalVisible(true);
+    this.setState({
+      isModalVisible: true,
+    })
+  }
+
+  handleOk = () => {
+    this.setState({
+      isModalVisible: false,
+    })
+  }
+
+  handleCancel = () => {
+    this.setState({
+      isModalVisible: false,
+    })
+  }
+
+  onInterValChange = (value) => {
+    this.setState({
+      fssExporterInterval: value,
+    })
+  }
+
+  onClusterChange = (value) => {
+    this.setState({
+      selectedCluster: value
+    });
   };
 
-  const handleOk = () => {
-    setIsModalVisible(false);
-  };
+  render() {
+    return (
+      <div className={styles.title}>
+        <Divider orientation="left">Prometheus配置下发</Divider>
 
-  const handleCancel = () => {
-    setIsModalVisible(false);
-  };
+        <p>拉取数据间隔(分钟)</p>
+        <Input placeholder="2" />
+        <div style={{ margin: '24px 0' }} />
 
-  return (
-    <div className={styles.title}>
-      <Divider orientation="left">Prometheus配置下发</Divider>
+        <Button type="primary">更新Prometheus.yml</Button>
+        <div style={{ margin: '24px 0' }} />
 
-      <p>拉取数据间隔(分钟)</p>
-      <Input placeholder="2" />
-      <div style={{ margin: '24px 0' }} />
+        <Divider orientation="left">FSS_Exporter配置下发</Divider>
+        <p>选择集群</p>
+        <ClusterSelect onChange={this.onClusterChange} />
+        <div style={{ margin: '24px 0' }} />
 
-      <Button type="primary">更新Prometheus.yml</Button>
-      <div style={{ margin: '24px 0' }} />
+        <p>查询间隔(分钟)</p>
+        <InputNumber
+          min={2}
+          max={60}
+          defaultValue={10}
+          onChange={this.onInterValChange}
+        />
+        <div style={{ margin: '24px 0' }} />
 
-      <Divider orientation="left">FSS_Exporter配置下发</Divider>
-      <p>选择集群</p>
-      <ClusterSelect onChange={} />
-      <div style={{ margin: '24px 0' }} />
+        <Button type="primary" onClick={this.showModal}>
+          更新FSS_Exporter.service
+        </Button>
+        <div style={{ margin: '24px 0' }} />
 
-      <p>查询间隔(分钟)</p>
-      <InputNumber
-        min={2}
-        max={60}
-        defaultValue={10}
-        onChange={onInterValChange}
-      />
-      <div style={{ margin: '24px 0' }} />
-
-      <Button type="primary" onClick={showModal}>
-        更新FSS_Exporter.service
-      </Button>
-      <div style={{ margin: '24px 0' }} />
-
-      <Modal
-        title="result Modal"
-        visible={isModalVisible}
-        onOk={handleOk}
-        onCancel={handleCancel}
-      >
-        <p>Some contents...</p>
-      </Modal>
-    </div>
-  );
-};
+        <Modal title="result" visible={this.state["isModalVisible"]} onOk={this.handleOk} onCancel={this.handleCancel}>
+          <p>Some contents...</p>
+        </Modal>
+      </div>
+    )
+  }
+}
